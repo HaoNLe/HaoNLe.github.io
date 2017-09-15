f1a06027c16d798df2afdda4c0e7248bf2937594
Hao Le <HaoLe@berkeley.edu>
2017-09-15 04:54:58 -0700

    Added carousel images Implemented carousel

--- scripts/script.js
+++ scripts/script.js
@@ -24,14 +24,14 @@ $(document).ready(function(){
   //if we click on something outside of navbar when its open, the navbar closes.
   $(document).click(function (event) {
         var navbarBurger = $navbarBurgers[0];
-        console.log($navbarBurgers);
-        console.log(navbarBurger);
+        //console.log($navbarBurgers);
+        //console.log(navbarBurger);
         // Get element we clicked on
         var clickover = $(event.target);
         // Check if burger is open
         var _opened = $(navbarBurger).hasClass("is-active");
-        console.log('opened: ' + _opened);
-        console.log('is-active: ' + clickover.hasClass("is-active"));
+        //console.log('opened: ' + _opened);
+        //console.log('is-active: ' + clickover.hasClass("is-active"));
         // if burger open AND element isn't our menu
         if (_opened === true && !clickover.hasClass("is-active")) {
             var target = $navbarBurgers[0].dataset.target;
@@ -63,7 +63,65 @@ $(document).ready(function(){
       });
     }
   });
+  
 
+
+
+  /* About me Carousel */
+  // get all dots
+  var $dots = Array.prototype.slice.call(document.querySelectorAll('.dot'), 0);
+  // Show initial image
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  // Check if there are any dots
+  if ($dots.length > 0) {
+
+    // Add a click event on each of the dots
+    $dots.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the slide number from id
+        var target = $el.id;
+
+        // Go to that slide
+        currentSlide(parseInt(target));
+      });
+    });
+  }
+
+  var $next = document.getElementById("forward");
+  $next.addEventListener('click', function() {
+    plusSlides(1);
+  });
+
+  var $prev = document.getElementById("backward");
+  $prev.addEventListener('click', function() {
+    plusSlides(-1);
+  });
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
 });
 /* Loading function */
 $(window).load(function() {
