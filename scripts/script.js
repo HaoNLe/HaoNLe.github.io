28b96d2a9525a608f07711169a871ffeb18038fd
Hao Le <HaoLe@berkeley.edu>
2018-10-20 00:06:18 -0700

    added in projects with transitinos and modals. Need to update with info and Modal images

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   /*  */
   $('.sidenav').sidenav();
   $('.carousel').carousel();
-
+  $('.modal').modal();
+          
   /* Add smooth scrolling to all links */
   $("a").on('click', function(event) {
 
